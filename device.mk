108751f43c460f69d557edcdbbefbf7a021f1af5
Arefin <ahmedarefinsajjad@gmail.com>
2023-01-26 14:28:43 +0600

    lavender: Proton bringup

--- device.mk
+++ device.mk
@@ -10,7 +10,6 @@ $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay \
